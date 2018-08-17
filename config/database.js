cb1f5e2c6fef3c6c79812573f24ece41fce10846
Nick Odumo <nicko@gmail.com>
2018-08-16 20:22:04 -0500

    first commit

---
+++ config/database.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const dotEnvPath = path.resolve('.env')
+
+/**
+ * since mocha don't see enviroment variables we have to use dotenv
+ */
+require('dotenv').config({ path: dotEnvPath })
+
+module.exports = {
+  development: {
+    'url': process.env.DATABASE_URL,
+    'dialect': 'postgres',
+    'define': {
+      'underscored': true
+    }
+  },
+  test: {
+    'url': process.env.DATABASE_URL_TEST,
+    'dialect': 'postgres',
+    'define': {
+      'underscored': true
+    }
+  },
+  staging: {
+    'url': process.env.DATABASE_URL_STAGING,
+    'dialect': 'postgres',
+    'define': {
+      'underscored': false
+    },
+    'ssl': true,
+    'dialectOptions': {
+      'ssl': {
+        'require': true
+      }
+    }
+  },
+  production: {
+    'url': process.env.DATABASE_URL_PRODUCTION,
+    'dialect': 'postgres',
+    'define': {
+      'underscored': false
+    },
+    'ssl': true,
+    'dialectOptions': {
+      'ssl': {
+        'require': true
+      }
+    }
+  }
+}
